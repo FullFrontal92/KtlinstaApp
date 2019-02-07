fb304ce47a80186c85c7810d33553d2056a99bfa
FullFrontal92 <adam.benyahia@hotmail.fr>
2019-02-07 19:57:30 +0100

    upload added

--- app/src/main/java/com/example/kotlininstagramfirebase/SignInActivity.kt
+++ app/src/main/java/com/example/kotlininstagramfirebase/SignInActivity.kt
@@ -1,9 +1,13 @@
 package com.example.kotlininstagramfirebase
 
+import android.app.Application
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SignInActivity : AppCompatActivity() {
 
@@ -23,9 +27,32 @@ class SignInActivity : AppCompatActivity() {
 
     fun sign_in(view : View){
 
+        mAuth!!.signInWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    val intent = Intent(applicationContext,FeedActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+
     }
 
     fun sign_up(view: View){
+
+        mAuth!!.createUserWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString())
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful){
+                    Toast.makeText(applicationContext,"User Created", Toast.LENGTH_LONG).show()
+                    val intent = Intent(applicationContext,FeedActivity::class.java)
+                    startActivity(intent)
+                }
+            }.addOnFailureListener { exception ->
+
+                if (exception != null) {
+                    Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+                }
+            }
 
     }
 }
