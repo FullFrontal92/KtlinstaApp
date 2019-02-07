b4217968af4978ebf8eb3e19e1a4c7239d3c0a15
FullFrontal92 <adam.benyahia@hotmail.fr>
2019-02-07 19:17:51 +0100

    firebase added

--- app/src/main/java/com/example/kotlininstagramfirebase/SignInActivity.kt
+++ app/src/main/java/com/example/kotlininstagramfirebase/SignInActivity.kt
@@ -2,11 +2,30 @@ package com.example.kotlininstagramfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+
+
+    var mAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListener = FirebaseAuth.AuthStateListener {  }
+    }
+
+
+    fun sign_in(view : View){
+
+    }
+
+    fun sign_up(view: View){
+
     }
 }
